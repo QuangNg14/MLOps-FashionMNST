7775e10f2892712b2235cda5243281a5207d09fa
quangnguyen <qnn1@rice.edu>
2024-06-15 18:57:29 -0400

    update load data

--- train.py
+++ train.py
@@ -131,39 +131,39 @@ def valid(model, valid_loader):
 
 
 # Data loading
-def load_data():
-    transform = transforms.Compose([transforms.ToTensor()])
-    train_dataset = datasets.FashionMNIST(
-        "./data", train=True, download=True, transform=transform
-    )
-    valid_dataset = datasets.FashionMNIST(
-        "./data", train=False, download=True, transform=transform
-    )
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle=False)
-    return train_loader, valid_loader
-
-
-# Data loading
 # def load_data():
 #     transform = transforms.Compose([transforms.ToTensor()])
-#     data_path = "./data"
-
-#     # Ensure data directory exists
-#     if not os.path.exists(data_path):
-#         os.makedirs(data_path)
-#         # Pull the dataset using DVC if not already available
-#         subprocess.run(["dvc", "pull", "data/FashionMNIST.dvc"])
-
 #     train_dataset = datasets.FashionMNIST(
-#         data_path, train=True, download=False, transform=transform
+#         "./data", train=True, download=True, transform=transform
 #     )
 #     valid_dataset = datasets.FashionMNIST(
-#         data_path, train=False, download=False, transform=transform
+#         "./data", train=False, download=True, transform=transform
 #     )
 #     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 #     valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle=False)
 #     return train_loader, valid_loader
+
+
+# Data loading
+def load_data():
+    transform = transforms.Compose([transforms.ToTensor()])
+    data_path = "./data"
+
+    # Ensure data directory exists
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+    # Pull the dataset using DVC if not already available
+    subprocess.run(["dvc", "pull"])
+
+    train_dataset = datasets.FashionMNIST(
+        data_path, train=True, download=False, transform=transform
+    )
+    valid_dataset = datasets.FashionMNIST(
+        data_path, train=False, download=False, transform=transform
+    )
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle=False)
+    return train_loader, valid_loader
 
 
 # Training and validation process
